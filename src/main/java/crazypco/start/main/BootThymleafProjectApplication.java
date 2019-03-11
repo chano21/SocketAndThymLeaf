92928c0fc160486f62ef415edce0d5a33ac785b2
chano <chano22@naver.com>
2019-03-12 02:36:19 +0900

    init

---
+++ src/main/java/crazypco/start/main/BootThymleafProjectApplication.java
@@ -0,0 +1,13 @@
+package crazypco.start.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootThymleafProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootThymleafProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/crazypco/test/start/HelloController.java
@@ -0,0 +1,23 @@
+package crazypco.test.start;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+   
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/viewTest")
+    public String viewTest(Model model){
+        model.addAttribute("message","Hello Spring Boot thymeleaf");
+        return "index";
+    }
+   
+}
