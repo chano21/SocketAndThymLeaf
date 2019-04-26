7d072f9aaecc136351fd6c50b63308f96b6bce47
chano <chano22@naver.com>
2019-04-26 16:59:51 +0900

    d

--- src/main/java/crazypco/start/main/controller/HelloController.java
+++ src/main/java/crazypco/start/main/controller/HelloController.java
@@ -3,7 +3,11 @@ package crazypco.start.main.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import crazypco.start.resetservice.RestServcie;
 
 @Controller
 public class HelloController {
@@ -11,14 +15,36 @@ public class HelloController {
     @RequestMapping("/")
     @ResponseBody
     public String index() {
-        return "Greetings from ddddddddzzzzzzzzzff Bootㅅtttdd!";
+        return "Greetings from ddddddddddddzzzzzzzzzff Bootㅅtttdd!";
     }
 
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    @ResponseBody
+    public String hello(Model model, @RequestParam(value = "test", defaultValue = "Unknown", required = false) String test) {
+
+    	System.out.println(test);
+    	 return "hello";
+    }
+
+    
     @RequestMapping("/viewTest")
     public String viewTest(Model model){
     	System.out.println("ㅎㅇㅎㅇdddd");
         model.addAttribute("message","Hello Spring Boot thymeleaf");
         return "index";
     }
+    
+    @RequestMapping("/RestTest")
+    public String RestTest(Model model){
+    	System.out.println("Hello thisis rest here");
+        
+    	String result=RestServcie.Get("http://www.naver.com");
+    	System.out.println(result);
+    //	model.addAttribute("message","Hello Spring Boot thymeleaf");
+        return "index";
+    }
+    
+    
+    
    
 }
